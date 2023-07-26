a7c8330f82af516d1ec5fcccfff38f65536a9121
Tankiso <zumatankiso1@gmail.com>
2023-07-26 12:23:56 -0700

    th

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - write line of code
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 int n;
@@ -9,7 +15,7 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* write your line of code here...
+* main -  write your line of code here...
 * Remember:
 * - you are not allowed to use a
 * - you are not allowed to modify p
